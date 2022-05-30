07d7fb2a13cdb90f5df006a4d15f603039d92c62
Luke Whiting <w019134i@student.staffs.ac.uk>
2022-05-30 14:55:06 +0100

    Working on deliverables

--- Endurance gamejam/Assets/Luke/Scripts/ObjectiveController.cs
+++ Endurance gamejam/Assets/Luke/Scripts/ObjectiveController.cs
@@ -6,7 +6,8 @@ public class ObjectiveController : MonoBehaviour
 {
     GameObject[] deliverableHouses;
 
-    public GameObject deliverableHouse;
+    [HideInInspector]
+    public DeliverableHouse deliverableHouse;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +16,20 @@ public class ObjectiveController : MonoBehaviour
         GenerateObjective();
     }
 
+    private void Update()
+    {
+        if (deliverableHouse)
+        {
+            if (deliverableHouse.delivered)
+            {
+                deliverableHouse.delivered = false;
+                GenerateObjective();
+            }
+        }
+    }
+
     public void GenerateObjective()
     {
-        deliverableHouse = deliverableHouses[Random.Range(0, deliverableHouses.Length)];
+        deliverableHouse = deliverableHouses[Random.Range(0, deliverableHouses.Length)].GetComponent<DeliverableHouse>();
     }
 }
