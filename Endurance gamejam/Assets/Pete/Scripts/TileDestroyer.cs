e465792162601bb237710bdfdfbf787c1bb3d7f9
kierpok <g018572k@student.staffs.ac.uk>
2022-05-30 16:14:29 +0100

    Added szymons player controller

--- Endurance gamejam/Assets/Pete/Scripts/TileDestroyer.cs
+++ Endurance gamejam/Assets/Pete/Scripts/TileDestroyer.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class TileDestroyer : MonoBehaviour
 {
+    private Quaternion startRotation;
+    private void Start()
+    {
+        startRotation = transform.rotation;
+    }
+    private void Update()
+    {
+        transform.rotation = startRotation;
+    }
     private void OnTriggerEnter(Collider other) { Destroy(other.gameObject); }
 
 } // class
