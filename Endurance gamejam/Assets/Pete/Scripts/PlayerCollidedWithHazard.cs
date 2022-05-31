0bceb4ec2feb31645f5c005a7ca26ea02d0f0fc6
kierpok <g018572k@student.staffs.ac.uk>
2022-05-31 11:54:23 +0100

    Hazards should be fixed with FX

--- Endurance gamejam/Assets/Pete/Scripts/PlayerCollidedWithHazard.cs
+++ Endurance gamejam/Assets/Pete/Scripts/PlayerCollidedWithHazard.cs
@@ -7,17 +7,20 @@ public class PlayerCollidedWithHazard : MonoBehaviour
     [SerializeField]
     private float knockbackValue = 100;
     [SerializeField]
-    private ParticleSystem collidedFX;
+    private GameObject collidedFX;
+    private bool doOnce = false;
 
     private void OnTriggerEnter(Collider other)
     {
-            if (other.tag == "Player") 
+            if (other.tag == "Player" && !doOnce) 
             {
                 other.gameObject.GetComponent<BikeAcceleration>().stop = true;
                 other.transform.GetChild(0).GetComponent<BikeSteering>().enabled = false;
                 other.transform.GetChild(0).GetComponent<Rigidbody>().useGravity = true;
-                other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(0, knockbackValue), Random.Range(0, knockbackValue), Random.Range(0, -knockbackValue)), ForceMode.Impulse);
-                collidedFX.Play();
+                other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(knockbackValue / 2, knockbackValue), Random.Range(knockbackValue / 2, knockbackValue), Random.Range(knockbackValue / 2, -knockbackValue)), ForceMode.Impulse);
+                GameObject temp = Instantiate(collidedFX, gameObject.transform);
+                temp.transform.localScale = new Vector3(5, 5, 5);
+                doOnce = true;
         }
     }
 
