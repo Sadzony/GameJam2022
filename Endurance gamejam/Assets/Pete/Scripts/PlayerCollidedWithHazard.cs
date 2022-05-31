6f62f9bcf40abdefa7522092ae188f6cca2e9758
kierpok <g018572k@student.staffs.ac.uk>
2022-05-31 10:18:08 +0100

    push

--- Endurance gamejam/Assets/Pete/Scripts/PlayerCollidedWithHazard.cs
+++ Endurance gamejam/Assets/Pete/Scripts/PlayerCollidedWithHazard.cs
@@ -4,21 +4,17 @@ using UnityEngine;
 
 public class PlayerCollidedWithHazard : MonoBehaviour
 {
-    private bool canGetKnockedback = false;
     [SerializeField]
     private float knockbackValue = 100;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (canGetKnockedback) 
-        {
             if (other.tag == "Player") 
             {
                 other.gameObject.GetComponent<BikeAcceleration>().stop = true;
                 other.transform.GetChild(0).GetComponent<BikeSteering>().enabled = false;
                 other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(0, knockbackValue), Random.Range(0, knockbackValue), Random.Range(0, -knockbackValue)), ForceMode.Impulse);  
             }
-        }
     }
 
 } // class
